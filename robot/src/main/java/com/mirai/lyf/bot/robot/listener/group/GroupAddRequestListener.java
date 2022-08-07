42fdbde4398f41b74e44389927a38e31bcf8c2a8
LYF_Uaena <lyf.aragaki@gmail.com>
2022-08-07 14:11:17 +0800

    群添加申请升级3.X

--- robot/src/main/java/com/mirai/lyf/bot/robot/listener/group/GroupAddRequestListener.java
+++ robot/src/main/java/com/mirai/lyf/bot/robot/listener/group/GroupAddRequestListener.java
@@ -1,19 +1,19 @@
 package com.mirai.lyf.bot.robot.listener.group;
 
 import com.mirai.lyf.bot.common.utils.PatternUtil;
+import com.mirai.lyf.bot.common.utils.StringUtils;
+import com.mirai.lyf.bot.persistence.domain.master.MemberInfo;
 import com.mirai.lyf.bot.persistence.domain.master.Roster;
+import com.mirai.lyf.bot.persistence.service.master.MemberService;
 import com.mirai.lyf.bot.persistence.service.master.RosterService;
 import lombok.extern.slf4j.Slf4j;
 import love.forte.simboot.annotation.Listener;
 import love.forte.simbot.ID;
+import love.forte.simbot.component.mirai.event.MiraiMemberJoinRequestEvent;
 import love.forte.simbot.definition.UserInfo;
-import love.forte.simbot.event.GroupJoinRequestEvent;
-import love.forte.simbot.event.GroupRequestEvent;
 import love.forte.simbot.event.RequestEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 /**
  * 监听群添加请求事件
@@ -24,18 +24,18 @@ public class GroupAddRequestListener {
 
     @Autowired
     private RosterService rosterService;
+    @Autowired
+    private MemberService memberService;
 
     /**
      * 群添加申请事件
-     *
-     * @param requestEvent the request event
      */
     @Listener
-    public void groupAddRequestListener(GroupRequestEvent requestEvent) {
+    public void groupAddRequestListener(MiraiMemberJoinRequestEvent event) {
         // 申请类型
-        RequestEvent.Type type = requestEvent.getType();
-        ID id = requestEvent.getRequester().getId();
-        String message = requestEvent.getMessage();
+        RequestEvent.Type type = event.getType();
+        ID id = event.getRequester().getId();
+        String message = event.getMessage();
         log.info("add request from: {}, message: {}, request type: {}", id, message, type);
         try {
             Thread.sleep(5000);
@@ -51,34 +51,27 @@ public class GroupAddRequestListener {
                 allowRequest = false;
             }
             if (allowRequest) {
-                String text = Optional.ofNullable(message).orElse("");
-                if (PatternUtil.getGroupAddInstance().matcher(text).find()) {
-                    requestEvent.acceptAsync();
-                    return;
+                if (PatternUtil.getGroupAddInstance().matcher(message).find()) {
+                    event.acceptAsync();
                 } else {
-                    requestEvent.rejectAsync();
-                    return;
+                    event.rejectAsync("请正确回答问题哦！如果不知道正确答案的话，请百度搜索‘彼岸图网’");
                 }
-//                sender.SETTER.setGroupAddRequest(
-//                        requestMsg.getFlag(),
-//                        PatternUtil.getGroupAddInstance().matcher(text).find(),
-//                        false,
-//                        "请正确回答问题哦！如果不知道正确答案的话，请百度搜索‘彼岸图网’"
-//                );
+                return;
             } else {
-                requestEvent.rejectAsync();
-//                sender.SETTER.setGroupAddRequest(requestMsg.getFlag(), false, false, "您曾经被踢出群聊，不允许重新加群！");
+                event.rejectAsync("您曾经被踢出群聊，不允许重新加群！");
             }
         }
         // 被邀请
-        if (RequestEvent.Type.INVITATION.compareTo(type) == 0 && requestEvent instanceof GroupJoinRequestEvent) {
-            UserInfo inviter = ((GroupJoinRequestEvent) requestEvent).getInviter();
-            requestEvent.rejectAsync();
+        if (RequestEvent.Type.INVITATION.compareTo(type) == 0) {
+            UserInfo inviter = event.getInviter();
+            if (inviter != null) {
+                ID inviterId = inviter.getId();
+                MemberInfo memberInfo = memberService.findByGroupCodeAndQqCode(event.getRequester().getFromGroupId(), Long.valueOf(inviterId.toString()));
+                if (StringUtils.equals("MEMBER", memberInfo.getIdentity())) {
+                    event.acceptAsync();
+                }
+            }
+            event.rejectAsync("不可通过邀请方式进群，请通过群号搜索添加");
         }
-//
-//        else {
-//            // 提示通过群号搜索添加
-//            sender.SETTER.setGroupAddRequest(requestMsg.getFlag(), false, false, "不可通过邀请方式进群，请通过群号搜索添加");
-//        }
     }
 }
