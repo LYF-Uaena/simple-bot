2c0c47f025aca69f308b215d25f7c6debcdadca9
LYF_UAENA <lyf.aragaki@gmail.com>
2023-07-02 01:48:52 +0800

    添加bot新注册方法

--- robot/src/main/java/com/mirai/lyf/bot/robot/RobotModule.java
+++ robot/src/main/java/com/mirai/lyf/bot/robot/RobotModule.java
@@ -1,6 +1,13 @@
 package com.mirai.lyf.bot.robot;
 
+
+import kotlin.Unit;
 import love.forte.simboot.spring.autoconfigure.EnableSimbot;
+import love.forte.simbot.application.Applications;
+import love.forte.simbot.component.mirai.MiraiComponent;
+import love.forte.simbot.component.mirai.bot.MiraiBotManager;
+import love.forte.simbot.core.application.Simple;
+import love.forte.simbot.core.application.SimpleApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,4 +21,13 @@ import org.springframework.context.annotation.Configuration;
 @EnableSimbot
 @ComponentScan("com.mirai.lyf.bot.robot")
 public class RobotModule {
+    public RobotModule() {
+
+        SimpleApplication application = Applications.buildSimbotApplication(Simple.INSTANCE)
+                .build((builder, config) -> {
+                    // 安装mirai组件
+                    builder.install(MiraiComponent.Factory, (config1, perceivable) -> Unit.INSTANCE);
+                    builder.install(MiraiBotManager.Factory, (config1, perceivable) -> Unit.INSTANCE);
+                }).createBlocking();
+    }
 }
